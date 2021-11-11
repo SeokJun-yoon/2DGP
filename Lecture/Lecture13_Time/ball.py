78e290378e4c512ba86e51baa77db786ad4ddf99
SeokJun-Yoon <sjyoon0@naver.com>
2021-11-11 18:44:46 +0900

    edit ball speed

--- Lecture/Lecture13_Time/ball.py
+++ Lecture/Lecture13_Time/ball.py
@@ -1,6 +1,12 @@
 from pico2d import *
 import game_world
 
+PIXEL_PER_METER = (10.0 / 5) # 10 pixel 5m
+BALL_SPEED_KMPH = 108.0 # Km / Hour
+BALL_SPEED_MPM = (BALL_SPEED_KMPH*1000.0/60.0)
+BALL_SPEED_MPS = (BALL_SPEED_MPM / 60.0)
+BALL_SPEED_PPS = (BALL_SPEED_MPS * PIXEL_PER_METER)
+
 class Ball:
     image = None
 
@@ -13,7 +19,7 @@ class Ball:
         self.image.draw(self.x, self.y)
 
     def update(self):
-        self.x += self.velocity
+        self.x += self.velocity+BALL_SPEED_PPS
 
         if self.x < 25 or self.x > 1600 - 25:
             game_world.remove_object(self)
